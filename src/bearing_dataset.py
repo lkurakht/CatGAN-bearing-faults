fad2f5f9e0b1299cb1a310da4b0ae00cf50b36d2
lkurakht <lkurakht@gmail.com>
2022-02-09 12:33:56 +0300

    Added custom classes types to dataset (A/B/C). Added classifier draft to noteboook

--- src/bearing_dataset.py
+++ src/bearing_dataset.py
@@ -17,24 +17,48 @@ def ScaleToSquare(arr: np.ndarray, size: int) -> np.ndarray:
 
 class BearingDataset(torch.utils.data.Dataset):
     def __init__(self, transform=None, target_transform=None, img_size=64, segment_size=1024, step=128, Nw=256, No=250,
-                 fs=12000.):
+                 fs=12000., type='A'):
         self.img_size = img_size
         self.segment_size = segment_size
         self.step = step
         self.Nw = Nw  # window width from 4.3 section
         self.No = No  # window overlap from 4.3 section
         self.fs = fs  # 12KHz
+        self.type = type # Let use only A, B, C datasets
 
-        # Let use only A, B, C datasets
-        self.labels = ['Normal', 'BA007', 'BA014', 'BA021', 'IR007', 'IR014', 'IR021', 'OR007', 'OR014', 'OR021']
-        self.filenames = ['../datasets/A/None/97.mat', '../datasets/A/BA/0.007/118.mat',
+        self.normal_label = 'Noprmal'
+        self.normal_filename = '../datasets/A/None/97.mat'
+        self.a_labels = [self.normal_label, 'BA007', 'BA014', 'BA021']
+        self.a_filenames = [self.normal_filename, '../datasets/A/BA/0.007/118.mat',
+                          '../datasets/A/BA/0.014/185.mat',
+                          '../datasets/A/BA/0.021/222.mat']
+        self.b_labels = [self.normal_label, 'BA007', 'IR007', 'OR007']
+        self.b_filenames = [self.normal_filename, '../datasets/A/BA/0.007/118.mat',
+                          '../datasets/C/IR/0.007/105.mat',
+                          '../datasets/C/OR/0.007/130.mat']
+        self.c_labels = [self.normal_label, 'BA007', 'BA014', 'BA021', 'IR007', 'IR014', 'IR021', 'OR007', 'OR014', 'OR021']
+        self.c_filenames = [self.normal_filename, '../datasets/A/BA/0.007/118.mat',
                           '../datasets/A/BA/0.014/185.mat',
                           '../datasets/A/BA/0.021/222.mat', '../datasets/C/IR/0.007/105.mat',
                           '../datasets/C/IR/0.014/169.mat',
                           '../datasets/C/IR/0.021/209.mat', '../datasets/C/OR/0.007/130.mat',
                           '../datasets/C/OR/0.014/197.mat',
                           '../datasets/C/OR/0.021/234.mat']
-        assert (len(self.labels) == len(self.filenames))
+        if self.type == 'A':
+            self.labels = self.a_labels
+            self.filenames = self.a_filenames
+        elif self.type == 'B':
+            self.labels = self.b_labels
+            self.filenames = self.b_filenames
+        elif self.type == 'C':
+            self.labels = self.c_labels
+            self.filenames = self.c_filenames
+        else:
+            raise ValueError('Unsupported dataset type.')
+
+        if len(self.labels) != len(self.filenames):
+            raise RuntimeError('labels and filenames number doesnt matchs')
+
         self.img_labels = []
         for idx, filename in enumerate(self.filenames):
             signal = loadmat(filename)
@@ -54,6 +78,12 @@ class BearingDataset(torch.utils.data.Dataset):
 
     def __getitem__(self, idx):
         return self.img_labels[idx]
+
+    def get_type(self):
+        return self.type
+
+    def get_cats_num(self):
+        return len(self.labels)
 
     def getlabel(self, idx):
         return self.labels[idx]
